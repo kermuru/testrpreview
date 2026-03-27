a263a094b1148b8f421bc7fb017ce5c5cb515cca
Kervin Fugata <151914582+kermuru@users.noreply.github.com>
2026-03-27 10:03:19 +0800

    envi to staging
    envi to staging

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -14,9 +14,9 @@ export const environment = {
         //apiUrl: 'https://wheat-capybara-997939.hostingersite.com/api',
 
         //live
-      apiUrl: 'https://api.rp-vespera.cloud/api',
+      //apiUrl: 'https://api.rp-vespera.cloud/api',
 
         //staging
-      //apiUrl: 'https://staging.rp-vespera.cloud/api',
+     apiUrl: 'https://staging.rp-vespera.cloud/api',
 
 };
